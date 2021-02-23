5cdcfc5913e0ed03ae24c9dd545ac3b50eda8997
sushil-cmd <sushilswain33@gmail.com>
2021-02-23 20:42:15 +0530

    adding templates with css

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = [
+    {
+        'author': 'Sushil',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date': 'Feb 23,2021',
+    },
+    {
+        'author': 'Priya',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date': 'Feb 23,2021',
+    },
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1.')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1.')
+    return render(request, 'blog/about.html', {'title': 'About'})
