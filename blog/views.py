d168bfbaa0e345f2628c1eb710a7c602a95052fe
sushil-cmd <sushilswain33@gmail.com>
2021-03-01 12:54:22 +0530

    added Post model to database

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author': 'Sushil',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date': 'Feb 23,2021',
-    },
-    {
-        'author': 'Priya',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date': 'Feb 23,2021',
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
